05b99748669f836892d897effab01a0359207ce4
murray16s <murray16s@ncssm.edu>
2016-02-03 16:27:41 -0500

    Adding in containers

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -1,19 +1,23 @@
-import javafx.stage.Stage;
 import javafx.application.Application;
-import java.io.IOException;
 import javafx.scene.Scene;
-//import javafx.scene.layout.Vbox;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 public class TicTacToe extends Application
 {
-    @Override
-    public void start(Stage primary)
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    //@Override
+    public void start(Stage primary) throws Exception
     {
         primary.setTitle("Tic-Tac-Toe");
+        VBox vbox = new VBox();
+        GridPane board = new GridPane();
+        vbox.getChildren().add(board);
+        Scene scene = new Scene(vbox, 800, 800);
+        primary.setScene(scene);
         primary.show();
-    }
-    public static void main(String [] args)
-    {
-        launch(args);
     }
 }
