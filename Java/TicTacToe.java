09609929aa2badf829a223eff15bab35eb0637e9
murray16s <murray16s@ncssm.edu>
2016-02-03 15:19:55 -0500

    adding basic fx framework

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -1,6 +1,19 @@
-
-
-public class TicTacToe 
+import javafx.stage.Stage;
+import javafx.application.Application;
+import java.io.IOException;
+import javafx.scene.Scene;
+//import javafx.scene.layout.Vbox;
+import javafx.scene.layout.GridPane;
+public class TicTacToe extends Application
 {
-    
+    @Override
+    public void start(Stage primary)
+    {
+        primary.setTitle("Tic-Tac-Toe");
+        primary.show();
+    }
+    public static void main(String [] args)
+    {
+        launch(args);
+    }
 }
