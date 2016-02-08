c53f5aea826c292a0bf46dde29c80c26894cbe12
murray16s <murray16s@ncssm.edu>
2016-02-08 11:25:35 -0500

    Adding MouseClick Event

--- Java/TicTacToeController.java
+++ Java/TicTacToeController.java
@@ -1,8 +1,10 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -32,10 +34,24 @@ public class TicTacToeController implements Initializable {
     @FXML
     private Menu fileMenu;
 
+    int rowIndex = 0;
+    int colIndex = 0;
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
         gridPane.setGridLinesVisible(true);
-
+    }
+    @FXML
+    private void mouseEntered(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        if(colIndex == null)
+            colIndex = 0;
+        if(rowIndex == null)
+            rowIndex = 0;
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex, rowIndex);
     }
 }
