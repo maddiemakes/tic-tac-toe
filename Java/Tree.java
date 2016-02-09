3b54883805caff9217cbceeb346ff05d742b899a
Samuel Murray <spamntaters@localhost.localdomain>
2016-02-09 14:02:32 -0500

    Adding base structure for Tree class

--- Java/Tree.java
+++ Java/Tree.java
@@ -2,5 +2,19 @@
 
 public class Tree 
 {
-    
+	Node root;
+	public Tree(Node root)
+	{
+		this.root = root;
+	}
+}
+class Node
+{
+	Node leftChild;
+	Node rightChild;
+	public Node(Node leftChild, Node rightChild)
+	{
+		this.rightChild = rightChild;
+		this.leftChild = leftChild;
+	}
 }
